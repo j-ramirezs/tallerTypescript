6eb2bb4747a0ca08fdbb2bfff391a2f4c146319f
Javier A. Ramírez <ja.ramirezs2@uniandes.edu.co>
2021-04-13 23:19:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tallerTypescript
+ Taller_Typescript1_ja.ramirezs2 - Javier Andrés Ramírez Silva - 201821781
---
+++ scripts/dataCourses.js
@@ -0,0 +1,11 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de SW en Equipos", "César Forero", 3),
+    new Course("Sistemas Transaccionales", "Anamaria Mojica", 3),
+    new Course("Lenguajes y Máquinas", "Nicolás Cardozo", 3),
+    new Course("TI en las Organizaciones", "Elizabeth Muñoz", 3),
+    new Course("Fenómenos de Transporte 2", "Andrés Gonzalez", 3),
+    new Course("Ingeniería de Reacciones", "Alicia Porras", 3),
+    new Course("CBU Colombia", "María Zuleta", 2),
+    new Course("CBU Apreciación del Sonido", "Catalina Peralta", 2)
+];
